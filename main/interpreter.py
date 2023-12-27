3388e793960e4379f11ef597c74ee4b203eac525
ITcomputer11 <keh09523@gmail.com>
2023-12-27 22:48:31 +0900

    Site Update. but we cant find hosting :(
    Co-authored-by: Kim Young Hyun <MAKEREKAM@users.noreply.github.com>

--- main/interpreter.py
+++ main/interpreter.py
@@ -1,21 +1,28 @@
 import logger
 import compute
 
-def Run(dir):
+def FileRun(dir):
     if not dir.lower().endswith(".eol"):
         logger.printError("0% 완료")
-        return
+        return "0% 완료"
     
     logger.printInfo(f'\"{dir}\" 가 실행되었습니다.')
     
+    code_list = []
+    
+    with open(dir, "r", encoding='UTF8') as f: # 
+        for line in f: code_list.append(line.strip())
+    
+    Run(code_list)
+
+def Run(code):
     code_list = []
     counter = 0
     accepted_list = list("나는.,빠름느림토끼달팽이느슨엄격춤춤망겜이라는게임을해 ")
     print_text = ""
     pointer = 0
     
-    with open(dir, "r", encoding='UTF8') as f: # 
-        for line in f: code_list.append(line.strip())
+    for line in code.split("\n"): code_list.append(line.strip())
     
     for line in code_list:
         counter+=1
@@ -23,18 +30,19 @@ def Run(dir):
         for com in list(line): # 알맞지 않은 문자가 들어가 있을 경우
             if com not in accepted_list:
                 logger.printError("과부하!")
-                return
+                return "과부하!"
         
-        if counter == 1 and line != "나는": # 오류 처리
+        if counter == 0 and line != "나는": # 오류 처리
             logger.printError("0% 완료")
-            return
+            return "0% 완료"
         elif counter == len(code_list):
-            if line != "이라는 게임을 해":
-                logger.printError("99% 완료")
-                return
-            else:
+            if line == "이라는 게임을 해":
                 logger.printing(print_text)
                 logger.printInfo("축하합니다!")
+                return f"{print_text}\n[info] 축하합니다!"
+            else:
+                logger.printError("99% 완료")
+                return "99% 완료"
         
         if line == "": continue 
 
@@ -55,4 +63,4 @@ def Run(dir):
                 logger.printing(pointer)
         except:
             logger.printError(f"{int(98 / counter)}% 완료")
-            return
+            return f"{int(98 / counter)}% 완료"
--- main/main.py
+++ main/main.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+import interpreter
+from flask import Flask, render_template, redirect, request, jsonify
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return redirect('https://imaginative-bonbon-a52d24.netlify.app/')
+
+@app.route('/interpreter', methods=['GET', 'POST'])
+def interpreterSite():
+    result = None
+    
+    if request.method == 'POST':
+        value = str(request.form['id_name'])
+        
+        result = f"\n{interpreter.Run(str(value))}".replace("\n", "<br/>")
+        return jsonify({'result': result})
+    else:
+        return render_template('Run.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
